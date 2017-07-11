c78eadeacbfb1ee7bef58e71b43195502c0b7728
mohamad ali <m.a.jafarzadeh@gmail.com>
2017-07-11 09:44:27 +0430

    test pull

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -4,7 +4,7 @@
 fun main(args: Array<String>) {
     val location = "World"
     val msg = "Hello' $location!"
-    for (n in (0..10).step(2).reversed())
+    for (n in (0..20).step(2).reversed())
         println(n)
     println(msg)
 }
